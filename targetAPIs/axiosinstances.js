8eaa03806d73f93e87839358cf1d0ba13272a6b2
ssearobber <ssearobber@naver.com>
2024-02-06 22:15:05 +0900

    first commit

--- targetAPIs/axiosinstances.js
+++ targetAPIs/axiosinstances.js
@@ -3,13 +3,13 @@ const qs = require('qs');
 require('dotenv').config();
 
 const shopifyServiceAxiosInstance = axios.create({
-  baseURL: process.env.SHOPIFY_SHOP_URL,
+  baseURL: process.env.SHOPIFY_SHOP_URL || shopifyShopUrl,
   timeout: 1000 * 10,
   paramsSerializer: function (params) {
     return qs.stringify(params, {arrayFormat: 'comma', encode: false});
   },
   headers: {
-    'X-Shopify-Access-Token': process.env.SHOPIFY_OAUTH_TOKEN,
+    'X-Shopify-Access-Token': process.env.SHOPIFY_OAUTH_TOKEN || shopifyOauthToken,
   },
 });
 
